8c800fe9bb5acd4b715fad6e3ee5c4bbdf92449a
j <j@jt14s>
2024-10-07 18:33:30 -0300

    add color nvim treesitter

--- .config/nvim/lua/tmpjg/colors.lua
+++ .config/nvim/lua/tmpjg/colors.lua
@@ -1,1 +1,15 @@
-vim.cmd.colorscheme("habamax") -- codedark 
+-- vim.cmd.colorscheme("habamax") -- codedark 
+
+
+-- Obtén la variable de entorno
+local HOST_ENVIRONMENT = os.getenv("HOST_ENVIRONMENT")
+
+-- Verifica el valor de la variable y aplica un esquema de colores
+if HOST_ENVIRONMENT == "work" then
+  vim.cmd.colorscheme("habamax")  -- Esquema de colores oscuro
+elseif HOST_ENVIRONMENT == "home" then
+  vim.cmd.colorsche("habamax")  -- Esquema de colores claro
+else
+  vim.cmd("colorscheme default")  -- Esquema de colores por defecto
+end
+
