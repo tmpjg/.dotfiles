0235be6d47fd48e1db6cce19d85ee2dc7504a8f9
j <j@jt14s>
2024-10-15 16:21:43 -0300

    kitty config

--- .config/nvim/lua/tmpjg/init.lua
+++ .config/nvim/lua/tmpjg/init.lua
@@ -5,3 +5,7 @@ require("tmpjg.colors")
 require("tmpjg.lazy")
 
 vim.opt.number = true
+
+vim.opt.tabstop = 4       -- Número de espacios que se consideran una tabulación
+vim.opt.shiftwidth = 4    -- Número de espacios para las auto-indentaciones
+vim.opt.expandtab = true   -- Usa espacios en lugar de tabulaciones
